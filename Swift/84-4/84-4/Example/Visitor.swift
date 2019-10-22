ce184e6f70a8c8add6c63b9364a04aeb41ef81c3
kimtaesu <kimtaesoo188@gmail.com>
2019-10-22 15:17:48 +0900

    Add JsonVisitor

--- Swift/84-4/84-4/Example/Visitor.swift
+++ Swift/84-4/84-4/Example/Visitor.swift
@@ -28,8 +28,53 @@ class ConsoleVisitor: Visitor {
         let isCompletedOut = task.isCompleted ? "[v]" : "[ ]"
         print("\(padding) \(isCompletedOut) \(task.title) (\(task.date))")
     }
-    
+
     func end(depth: Int) {
-        
+
+    }
+}
+
+/*
+ 큰 흐름은 Template method pattern 으로 강제를 시키고
+ 안에 몸통은 유연하게 갖도록하는 것들은 Visitor pattern을 사용한다.
+ 
+ Android 에서 Acitivity Lifecycle의
+ [onCreate, onResume, onPause] 등은 Lifecycle 타는 거대한 Visitor 다.
+ 
+ Renderer 는 Strategy 객체를 공급한 결과가 된다.
+ 그렇다면 큰 범주에서 보면 Visitor pattern 도 Strategy pattern 일까?
+ 
+ Visitor VS Strategy
+ 제어 능력을 잃어버린 객체는 Visitor 라고 부른다.
+ 
+ 제어 역전은 참조를 거꿀로 하는 것이 아니다.
+ 
+ 의존성 역전과 제어역전의 의미는 다르다.
+ 
+ 의존성 역전은 구상 객체를 아는 것이 아니라 구상 객체의 추상 객체를 아는 것이다.
+ 제어 역전(IoC)은 제어 구문이 Lifecycle 을 가지고 있는 객체를 소비하는 것이다.
+ */
+class JsonVisitor: Visitor {
+
+    func getPadding(depth: Int) -> String {
+        var padding = ""
+        (0..<depth).forEach { _ in
+            padding += "   "
+        }
+        return padding
+    }
+    func drawTask(task: CompositeTask, depth: Int) {
+        let padding = getPadding(depth: depth)
+        print("\(padding) {")
+        print("\(padding)  title: \"\(task.title)\",")
+        print("\(padding)  date: \"\(task.date)\",")
+        print("\(padding)  isComplete: \"\(task.isCompleted)\",")
+        print("\(padding)  sub: [ ")
+    }
+
+    func end(depth: Int) {
+        let padding = getPadding(depth: depth)
+        print("\(padding)  ]")
+        print("\(padding)  },")
     }
 }
--- Swift/84-4/84-4Tests/_4_4Tests.swift
+++ Swift/84-4/84-4Tests/_4_4Tests.swift
@@ -11,7 +11,7 @@ import XCTest
 
 class _4_4Tests: XCTestCase {
 
-    func testExample() {
+    func testConsoleVisitor() {
         let root = CompositeTask(title: "Root", date: Date())
         root.addTask(title: "sub1", date: Date())
         root.addTask(title: "sub2", date: Date())
@@ -38,6 +38,77 @@ class _4_4Tests: XCTestCase {
          - [ ] sub2 (2019-10-22 06:01:44 +0000)
          -- [ ] sub2_1 (2019-10-22 06:01:44 +0000)
          -- [ ] sub2_2 (2019-10-22 06:01:44 +0000)
+         */
+    }
+    func testJsonVisitor() {
+        let root = CompositeTask(title: "Root", date: Date())
+        root.addTask(title: "sub1", date: Date())
+        root.addTask(title: "sub2", date: Date())
+        
+        let report: TaskReport = root.getReport(sortType: CompositeSortType.title_asc)
+        let sub1 = report.tasks[0].task
+        let sub2 = report.tasks[1].task
+        
+        sub1.addTask(title: "sub1_1", date: Date())
+        sub1.addTask(title: "sub1_2", date: Date())
+        
+        sub2.addTask(title: "sub2_1", date: Date())
+        sub2.addTask(title: "sub2_2", date: Date())
+        
+        let renderer1 = Renderer(factory: { JsonVisitor() })
+        renderer1.render(report: root.getReport(sortType: CompositeSortType.title_asc))
+         /*
+         실행 결과
+         
+         {
+         title: "Root",
+         date: "2019-10-22 06:17:05 +0000",
+         isComplete: "false",
+         sub: [
+           {
+            title: "sub1",
+            date: "2019-10-22 06:17:05 +0000",
+            isComplete: "false",
+            sub: [
+              {
+               title: "sub1_1",
+               date: "2019-10-22 06:17:05 +0000",
+               isComplete: "false",
+               sub: [
+               ]
+               },
+              {
+               title: "sub1_2",
+               date: "2019-10-22 06:17:05 +0000",
+               isComplete: "false",
+               sub: [
+               ]
+               },
+            ]
+            },
+           {
+            title: "sub2",
+            date: "2019-10-22 06:17:05 +0000",
+            isComplete: "false",
+            sub: [
+              {
+               title: "sub2_1",
+               date: "2019-10-22 06:17:05 +0000",
+               isComplete: "false",
+               sub: [
+               ]
+               },
+              {
+               title: "sub2_2",
+               date: "2019-10-22 06:17:05 +0000",
+               isComplete: "false",
+               sub: [
+               ]
+               },
+            ]
+            },
+         ]
+         },
          */
     }
 }
